edd6d34f2bb9a32424a399e803481d3d16bf17e5
xieruibiao <1137883309@qq.com>
2021-02-01 16:29:43 +0800

    修改导航栏上一段空白

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <a-layout id="components-layout-demo-top-side-2">
+
+  <div>
+    <!-- <div class="head"></div>
+        <span style="font-family:KaiTi;float:left;margin-left:30px;font-size:25px;color:orange">
+              基于web的交互电子书
+        </span>
+
+      <span style="font-family:SimHei;float:left;margin-left:150px;font-size:20px;color:black">
+               <a-icon type="home" />首页
+        </span>
+     <span style="font-family:SimHei;float:left;margin-left:50px;font-size:20px;color:black">
+              电子课本
+        </span>
+    </div> -->
+
+
+
+
+
+<a-layout id="components-layout-demo-top-side-2">
     <a-layout-header class="header">
       <div class="logo" />
       <a-menu
@@ -85,6 +104,9 @@
       </a-layout>
     </a-layout>
   </a-layout>
+  </div>
+
+  
 </template>
 <script>
 export default {
@@ -109,5 +131,20 @@ export default {
 .header {
   position: relative;
   top : auto;
+}
+
+.head {
+  height: 80px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  margin-top: 10px;
+  border: 1ch;
+}
+
+.content {
+  height: 1400px;
+  background: orange;
+  margin-top: 80px;
 }
 </style>
--- src/view/editBook/EditContent.vue
+++ src/view/editBook/EditContent.vue
@@ -55,7 +55,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 
 .quill-editor {
